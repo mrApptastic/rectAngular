cfe4db80a7cdb643454ef81f541da3e48147b8ba
mrAppTastic <henrikbeske@gmail.com>
2019-06-19 11:32:28 +0200

    Added line left and score logic and change unfilled rect to stroke instead of fill.

--- rectAngular.js
+++ rectAngular.js
@@ -9,14 +9,15 @@
 		"y2" : null,
 		"tx" : null,
 		"ty" : null,
-		"left" : 1000,
-		"score" : 0
+		"left" : 10000,
+		"score" : 0,
+		"start" : null
 	};
 	rect.ball = {
 		"x" : 20,
 		"y" : 20,
 		"w" : 20,
-		"mx" : true, 
+		"mx" : true,		
 		"my" : true,
 		"speed" : 1,
 		"step" : 1,
@@ -39,31 +40,52 @@
 		if (!rect.set.ball) {
 			rect.set.ball = JSON.stringify(rect.ball);
 		}
+		/* Set Start Date */
+		if (!rect.draw.start) {
+			rect.draw.start = new Date();
+		} 
 		rect.area.width = width ? width : window.innerWidth;
 		rect.area.height = height ? height : window.innerHeight;
 		rect.area.style.cursor = "none";
 		rect.ctxt.fillStyle = "DodgerBlue";
+		rect.ctxt.strokeStyle = "DodgerBlue";
+		rect.ctxt.font = "30px Verdana";
 		rect.anima = setInterval(rect.paint, 1);
 	};
 	rect.area.onclick = function (e) {
-		var x = e.pageX - (rect.area.getBoundingClientRect().left + window.scrollX);
-		var y = e.pageY - (rect.area.getBoundingClientRect().top + window.scrollY);
-		/* No Clicks */
-		if (!rect.draw.x1 && !rect.draw.y1 && !rect.draw.x1 && !rect.draw.y1) {			
-			rect.draw.x1 = x;
-			rect.draw.y1 = y;
+		if (rect.anima) {
+			let x = e.pageX - (rect.area.getBoundingClientRect().left + window.scrollX);
+			let y = e.pageY - (rect.area.getBoundingClientRect().top + window.scrollY);
+		
+			/* No Clicks */
+			if (!rect.draw.x1 && !rect.draw.y1 && !rect.draw.x1 && !rect.draw.y1) {			
+				rect.draw.x1 = x;
+				rect.draw.y1 = y;
+			}
+			/* One Click */
+			else if (rect.draw.x1 && rect.draw.y1 && !rect.draw.x2 && !rect.draw.y2) {
+				rect.draw.x2 = x;
+				rect.draw.y2 = y;
+			
+				/* Modify Line Left */
+				let x_1 = rect.draw.x1 > rect.draw.x2 ? rect.draw.x2 : rect.draw.x1;
+				let x_2 = rect.draw.x1 > rect.draw.x2 ? rect.draw.x1 : rect.draw.x2;
+				let y_1 = rect.draw.y1 > rect.draw.y2 ? rect.draw.y2 : rect.draw.y1;
+				let y_2 = rect.draw.y1 > rect.draw.y2 ? rect.draw.y1 : rect.draw.y2;
+				let x_L = x_2 - x_1;
+				let y_L = y_2 - y_1;
+				rect.draw.left -= (x_L + y_L);
+			}
+			/* Two Clicks */
+			else if (rect.draw.x1 && rect.draw.y1 && rect.draw.x2 && rect.draw.y2) {
+				rect.draw.x1 = x;
+				rect.draw.y1 = y;
+				rect.draw.x2 = null;
+				rect.draw.y2 = null;
+			}
 		}
-		/* One Click */
-		else if (rect.draw.x1 && rect.draw.y1 && !rect.draw.x2 && !rect.draw.y2) {
-			rect.draw.x2 = x;
-			rect.draw.y2 = y;
-		}
-		/* Two Clicks */
-		else if (rect.draw.x1 && rect.draw.y1 && rect.draw.x2 && rect.draw.y2) {
-			rect.draw.x1 = x;
-			rect.draw.y1 = y;
-			rect.draw.x2 = null;
-			rect.draw.y2 = null;
+		else {
+			rect.init();
 		}
 	};
 	rect.area.onmousemove = function (e) {
@@ -71,8 +93,22 @@
 		rect.draw.ty = e.pageY - (rect.area.getBoundingClientRect().top + window.scrollY);
 	};
 	rect.paint = function () {
+		/* Check if there is line left */
+		if (rect.draw.left < 0) {
+			// End Logic Goes here
+		}
+		
 		/* Clear Canvas */
-		rect.ctxt.clearRect(0, 0, window.innerWidth, window.innerHeight);
+		rect.ctxt.clearRect(0, 0, rect.area.width, rect.area.height);
+		
+		/* Update Score */
+		rect.draw.score = rect.draw.start ? Math.floor((new Date().getTime() - rect.draw.start.getTime()) / 1000) : 0;
+		
+		/* Display Line Left */
+		rect.ctxt.fillText("Line: " + rect.draw.left, rect.area.width - 190, 35);	
+		
+		/* Display Score */
+		rect.ctxt.fillText("Score: " + rect.draw.score, 10, 35);
 		
 		/* Draw Rect (Mouse) */
 		if (rect.draw.tx && rect.draw.ty) {
@@ -80,7 +116,7 @@
 				rect.ctxt.fillRect(rect.draw.tx, rect.draw.ty, 5, 5);
 			}
 			else if (rect.draw.x1 && rect.draw.y1 && !rect.draw.x2 && !rect.draw.y2) {
-				rect.ctxt.fillRect(rect.draw.x1, rect.draw.y1, -(rect.draw.x1 - rect.draw.tx), -(rect.draw.y1 - rect.draw.ty));
+				rect.ctxt.strokeRect(rect.draw.x1, rect.draw.y1, -(rect.draw.x1 - rect.draw.tx), -(rect.draw.y1 - rect.draw.ty));
 			}
 			else if (rect.draw.x1 && rect.draw.y1 && rect.draw.x2 && rect.draw.y2) {
 				rect.ctxt.fillRect(rect.draw.tx, rect.draw.ty, 5, 5);
@@ -142,18 +178,7 @@
 			let ny_2 = y_2 + rect.ball.w;
 					
 			/* Direction Up-Left */
-			if (!rect.ball.mx && !rect.ball.my) {
-				
-				console.log({
-				bx : rect.ball.x,
-				by : rect.ball.y,
-				x1 : mx_1,
-				x2 : mx_2,
-				y1 : my_1,
-				y2 : my_2,
-				
-				});
-				
+			if (!rect.ball.mx && !rect.ball.my) {				
 				if (nx_1 <= rect.ball.x && nx_2 >= rect.ball.x && ny_1 <= rect.ball.y && ny_2 >= rect.ball.y) {
 					let fromX = rect.ball.x - nx_2;
 					let fromY = rect.ball.y - ny_2;
